143299e6b2cd44d87108026c0ff65cb60c01c9dc
Guillermo Alarcón <xxmgaxx@gmail.com>
2018-04-12 14:31:34 +0200

    fix: model

---
+++ src/app/shared/model/user-details.model.ts
@@ -0,0 +1,7 @@
+export class UserDetails {
+    sex: string;
+    weight: number;
+    stature: number;
+    age: number;
+    activity: string;
+}
--- src/app/shared/model/user.model.ts
+++ src/app/shared/model/user.model.ts
@@ -1,13 +1,8 @@
+import { UserDetails } from "./user-details.model";
 export class User {
     id: string;
     email: string;
     password?: string;
     nickname: string;
-    info: {
-        sex: string,
-        weight: number,
-        stature: number,
-        age: number,
-        activity: string
-    } = {} as any;
+    info: UserDetails = new UserDetails();
 }
